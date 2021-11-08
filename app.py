d480770a9a5a0fc8fbde50565ea0c93fd9723884
LongDefeat <78603508+LongDefeat@users.noreply.github.com>
2021-11-08 15:34:29 -0800

    practice with python and flask
    this project included a requirements.txt file that I will remember to use for future projects

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/welcome')
+def say_welcome():
+    return """
+        <h1> Welcome, User!</h1>
+    """
+
+@app.route('/welcome/home')
+def welcome_home():
+    return """
+        <h1> Welcome, Home!!</h1>
+    """
+
+@app.route('/welcome/back')
+def welcome_back():
+    return """
+        <h1> Welcome back!!</h1>
+    """
